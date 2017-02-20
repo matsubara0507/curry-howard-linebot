d62efafffc414ffae48c4325dbaec541eb4fce9a
松原信忠 <t12307043@gunma-u.ac.jp>
2017-02-20 21:54:49 +0900

    Added Curry-Howard correspondence

--- app/CurryHowardCorrespondence.hs
+++ app/CurryHowardCorrespondence.hs
@@ -35,9 +35,18 @@ safeHead = find (const True)
 curryHowardCorrespondence :: [([Text], [Text], URL)]
 curryHowardCorrespondence =
   [ ( ["Natural Deducation", "自然演繹"]
-    , ["Typed Lambda Calclus", "型付きラムダ計算"]
+    , ["Typed Lambda Calclus", "型付きラムダ計算", "単純型付きラムダ計算", "型付きλ計算", "単純型付きλ計算"]
     , "http://disi.unitn.it/~bernardi/RSISE11/Papers/curry-howard.pdf")
   , ( ["Sequent Calculus", "シーケント計算"]
-    , ["Typed Lambda Calclus", "型付きラムダ計算"]
+    , ["Typed Lambda Calclus", "型付きラムダ計算", "単純型付きラムダ計算", "型付きλ計算", "単純型付きλ計算"]
     , "http://disi.unitn.it/~bernardi/RSISE11/Papers/curry-howard.pdf")
+  , ( ["System F"]
+    , ["Polymorphic Lambda Calculus", "2階ラムダ計算", "多相ラムダ計算", "2階λ計算", "多相λ計算"]
+    , "http://disi.unitn.it/~bernardi/RSISE11/Papers/curry-howard.pdf")
+  , ( ["Modal Logic", "様相論理"]
+    , ["Mondad", "モナド"]
+    , "http://www.sciencedirect.com/science/article/pii/S0304397596001697")
+  , ( ["Linear Logic", "線形論理"]
+    , ["Session Type", "セッション型"]
+    , "http://ctp.di.fct.unl.pt/~btoninho/mscs12.pdf")
   ]
